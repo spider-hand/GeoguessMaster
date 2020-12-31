9c99b1736bf376307b4609546497d7cbf9ed6e8e
Spider Hand <creative.spider.hand@gmail.com>
2020-12-31 16:03:59 +0900

    Define viewport in App component and provide it to children components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
-  <v-app>
+  <div>
     <router-view />
-  </v-app>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'App',
-  data: () => ({
-    //
-  }),
-};
+<script lang="ts">
+import { defineComponent, provide, } from '@vue/composition-api'
+
+import { Viewport } from '@/types/index'
+
+export default defineComponent({
+
+  setup() {
+    const updateSizes = (obj: any = {}): Viewport  => {
+      obj.width = window.innerWidth
+      obj.height = window.innerHeight
+      return obj
+    }
+
+    provide('viewport', updateSizes())
+  }
+})
 </script>
 
 <style>
