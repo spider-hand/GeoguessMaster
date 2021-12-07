064b3e33042edf5e7e2691bf0d73692faf0c2f3a
spider-hand <creative.spider.hand@gmail.com>
2021-12-07 20:47:18 +0900

    Remove lint warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,9 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": ["error",{
+      "endOfLine": "auto"}
+    ],
   },
   overrides: [
     {
