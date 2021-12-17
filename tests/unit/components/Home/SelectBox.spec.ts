ccbb2b28327b27454e97ffb63d838fd811c60ef0
spider-hand <creative.spider.hand@gmail.com>
2021-12-18 03:35:23 +0900

    Add test cases

--- tests/unit/components/Home/SelectBox.spec.ts
+++ tests/unit/components/Home/SelectBox.spec.ts
@@ -1,6 +1,7 @@
 import { mount, VueWrapper } from "@vue/test-utils";
 
 import SelectBox from "@/components/Home/SelectBox.vue";
+import SelectboxDialog from "@/components/Home/SelectBoxDialog.vue";
 import { MAP_OPTIONS } from "@/constants";
 
 describe("Test SelectBox component", () => {
@@ -46,6 +47,22 @@ describe("Test SelectBox component", () => {
     expect(wrapper.find(".select-box-dialog").exists()).toBe(true);
   });
 
+  it("Dialog should disappear when clicking outside component", async () => {
+    await wrapper.find(".select-box-container").trigger("click");
+
+    await window.document.body.dispatchEvent(new Event('click'));
+
+    expect(wrapper.find(".select-box-dialog").exists()).toBe(false);
+  });
+
+  it("Dialog should not disapper when clicking the dialog itself", async () => {
+    await wrapper.find(".select-box-container").trigger("click");
+
+    await wrapper.find(".select-box-dialog").trigger("click");
+
+    expect(wrapper.find(".select-box-dialog").exists()).toBe(true);
+  });
+
   it("Test the style on hovering the mouse", async () => {
     await wrapper.find(".select-box-container").trigger("mouseover");
 
@@ -67,5 +84,12 @@ describe("Test SelectBox component", () => {
     expect(wrapper.find(".select-box-container").classes()).not.toContain(
       "on-hover-select-box-container"
     );
+  });
+
+  it("Test onChangeOption", () => {
+    const selectBoxDialog = wrapper.findComponent(SelectboxDialog);
+    selectBoxDialog.vm.$emit("onChangeOption", { text: "Japan", value: "JPN" });
+
+    expect(wrapper.emitted().onChangeOption).toBeDefined();
   });
 });
