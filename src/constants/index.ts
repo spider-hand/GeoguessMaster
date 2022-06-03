038a16a05d3ee58cb0ed8180116961ce5fc75e11
spider-hand <creative.spider.hand@gmail.com>
2022-06-03 23:30:58 +0900

    Remove geo json files which don't work well

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,34 +5,10 @@ export const MAP_OPTIONS: SelectboxOption[] = [
     text: "World",
     value: "WORLD",
   },
-  /**
-  {
-    text: "France",
-    value: "FRA",
-  },
-  {
-    text: "Germany",
-    value: "DEU",
-  },
-  {
-    text: "India",
-    value: "IND",
-  },
-*/
   {
     text: "Japan",
     value: "JPN",
   },
-  /** 
-  {
-    text: "South Korea",
-    value: "KOR",
-  },
-  {
-    text: "United States",
-    value: "USA",
-  },
-*/
 ];
 
 export const MODE_OPTIONS: SelectboxOption[] = [
